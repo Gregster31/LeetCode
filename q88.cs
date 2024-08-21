629f803a24c86efde9190fd7052a7a4365c4c86d
Julien <julienhalde9@gmail.com>
2024-08-21 15:37:03 -0400

    Create q88.cs

---
+++ q88.cs
@@ -0,0 +1,20 @@
+public class Solution {
+public void Merge(int[] nums1, int m, int[] nums2, int n) {
+    int i = m -1, j = n - 1, k = m + n -1;
+
+    // MErge your array in reverse direction
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k--] = nums1[i--];
+        } else {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
+    // If there are still nums in nums2[]
+    while (j >= 0) {
+        nums1[k--] = nums2[j--];
+    }
+
+ }
+}
